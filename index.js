c2120762d4f5e1b5b6f8d3bca55bb9f2dd46f18a
LenAnderson <Anderson.Len@outlook.com>
2024-06-18 21:24:20 -0400

    fix for keyboard enabled buttons and shit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const textarea = document.querySelector('#send_textarea');
 window.addEventListener('keydown', (evt)=>{
     if (document.activeElement == textarea) return;
-    if (document.activeElement != document.body) return;
+    if (['INPUT', 'SELECT', 'TEXTAREA'].includes(document.activeElement.tagName)) return;
     if (evt.key == ' ' || evt.key.length > 1 || evt.ctrlKey || evt.altKey) return;
     const rect = textarea.getBoundingClientRect();
     if (document.elementFromPoint(rect.left + rect.width / 2, rect.top + rect.height / 2) != textarea) return;
